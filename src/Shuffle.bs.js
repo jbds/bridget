bff9ab00590ae28b8e6e99acc689a54600e1be56
Jon Beckett <sales@jbds.co.uk>
2020-07-07 15:04:23 +0100

    refactor

--- src/Shuffle.bs.js
+++ src/Shuffle.bs.js
@@ -615,38 +615,10 @@ function pocAsString(poc) {
 }
 
 function optionIntAsString(level) {
-  if (level === undefined) {
+  if (level !== undefined) {
+    return String(level);
+  } else {
     return "";
-  }
-  switch (level) {
-    case 1 :
-        return "1";
-    case 2 :
-        return "2";
-    case 3 :
-        return "3";
-    case 4 :
-        return "4";
-    case 5 :
-        return "5";
-    case 6 :
-        return "6";
-    case 7 :
-        return "7";
-    case 8 :
-        return "8";
-    case 9 :
-        return "9";
-    case 10 :
-        return "10";
-    case 11 :
-        return "11";
-    case 12 :
-        return "12";
-    case 13 :
-        return "13";
-    default:
-      return "X";
   }
 }
 
