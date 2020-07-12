5179c71994a8affd50e9ed678a9ca293355e3bbe
Jon Beckett <sales@jbds.co.uk>
2020-07-12 17:55:54 +0100

    bidding window button constraints completed

--- src/Components/BiddingWindow.bs.js
+++ src/Components/BiddingWindow.bs.js
@@ -12,55 +12,114 @@ function BiddingWindow(Props) {
   var state = Props.state;
   var bidListHead = Belt_List.head(state.bids);
   var isShowXAndXX = bidListHead !== undefined ? bidListHead.contractSuit !== "" : false;
+  var bidsListFilteredByContractLevel = Belt_List.keep(state.bids, (function (x) {
+          return x.contractLevel !== undefined;
+        }));
+  var optionCurrentContractLevelBid = Belt_List.head(bidsListFilteredByContractLevel);
+  var optionCurrentContractLevel = optionCurrentContractLevelBid !== undefined ? optionCurrentContractLevelBid.contractLevel : undefined;
+  var currentContractLevel = optionCurrentContractLevel !== undefined ? optionCurrentContractLevel : 0;
+  console.log("currentContractLevel:");
+  console.log(currentContractLevel);
+  var optionCurrentContractSuit = optionCurrentContractLevelBid !== undefined ? optionCurrentContractLevelBid.contractSuit : undefined;
+  var currentContractSuitAsString = optionCurrentContractSuit !== undefined ? optionCurrentContractSuit : "";
+  var currentContractSuitAsRank;
+  switch (currentContractSuitAsString) {
+    case "Clubs" :
+        currentContractSuitAsRank = 0;
+        break;
+    case "Diamonds" :
+        currentContractSuitAsRank = 1;
+        break;
+    case "Hearts" :
+        currentContractSuitAsRank = 2;
+        break;
+    case "NoTrumps" :
+        currentContractSuitAsRank = 4;
+        break;
+    case "Spades" :
+        currentContractSuitAsRank = 3;
+        break;
+    default:
+      currentContractSuitAsRank = -1;
+  }
+  console.log("currentContractSuitAsRank:");
+  console.log(currentContractSuitAsRank);
+  var optionBidListTail = Belt_List.tail(bidsListFilteredByContractLevel);
+  var bidListTail = optionBidListTail !== undefined ? optionBidListTail : /* [] */0;
+  var optionPreviousBid = Belt_List.head(bidListTail);
+  var optionPreviousContractSuit = optionPreviousBid !== undefined ? optionPreviousBid.contractSuit : undefined;
+  var previousContractSuitAsString = optionPreviousContractSuit !== undefined ? optionPreviousContractSuit : "";
+  var previousContractSuitAsRank;
+  switch (previousContractSuitAsString) {
+    case "Clubs" :
+        previousContractSuitAsRank = 0;
+        break;
+    case "Diamonds" :
+        previousContractSuitAsRank = 1;
+        break;
+    case "Hearts" :
+        previousContractSuitAsRank = 2;
+        break;
+    case "NoTrumps" :
+        previousContractSuitAsRank = 4;
+        break;
+    case "Spades" :
+        previousContractSuitAsRank = 3;
+        break;
+    default:
+      previousContractSuitAsRank = -1;
+  }
+  console.log("previousContractSuitAsRank:");
+  console.log(previousContractSuitAsRank);
   return React.createElement(React.Fragment, undefined, React.createElement(ButtonBidSmall$ReasonReactExamples.make, {
                   dispatch: dispatch,
                   action: /* BidAdd */Block.__(2, [1]),
                   label: "1",
                   id: "btnBid1",
                   color: "#606060",
-                  isVisible: state.isBiddingHideDenominationButtons
+                  isVisible: state.isBiddingHideDenominationButtons && (currentContractLevel <= 1 && currentContractSuitAsRank !== 4 || currentContractLevel < 1 && currentContractSuitAsRank === 4)
                 }), React.createElement(ButtonBidSmall$ReasonReactExamples.make, {
                   dispatch: dispatch,
                   action: /* BidAdd */Block.__(2, [2]),
                   label: "2",
                   id: "btnBid2",
                   color: "#606060",
-                  isVisible: state.isBiddingHideDenominationButtons
+                  isVisible: state.isBiddingHideDenominationButtons && (currentContractLevel <= 2 && currentContractSuitAsRank !== 4 || currentContractLevel < 2 && currentContractSuitAsRank === 4)
                 }), React.createElement(ButtonBidSmall$ReasonReactExamples.make, {
                   dispatch: dispatch,
                   action: /* BidAdd */Block.__(2, [3]),
                   label: "3",
                   id: "btnBid3",
                   color: "#606060",
-                  isVisible: state.isBiddingHideDenominationButtons
+                  isVisible: state.isBiddingHideDenominationButtons && (currentContractLevel <= 3 && currentContractSuitAsRank !== 4 || currentContractLevel < 3 && currentContractSuitAsRank === 4)
                 }), React.createElement(ButtonBidSmall$ReasonReactExamples.make, {
                   dispatch: dispatch,
                   action: /* BidAdd */Block.__(2, [4]),
                   label: "4",
                   id: "btnBid4",
                   color: "#606060",
-                  isVisible: state.isBiddingHideDenominationButtons
+                  isVisible: state.isBiddingHideDenominationButtons && (currentContractLevel <= 4 && currentContractSuitAsRank !== 4 || currentContractLevel < 4 && currentContractSuitAsRank === 4)
                 }), React.createElement(ButtonBidSmall$ReasonReactExamples.make, {
                   dispatch: dispatch,
                   action: /* BidAdd */Block.__(2, [5]),
                   label: "5",
                   id: "btnBid5",
                   color: "#606060",
-                  isVisible: state.isBiddingHideDenominationButtons
+                  isVisible: state.isBiddingHideDenominationButtons && (currentContractLevel <= 5 && currentContractSuitAsRank !== 4 || currentContractLevel < 5 && currentContractSuitAsRank === 4)
                 }), React.createElement(ButtonBidSmall$ReasonReactExamples.make, {
                   dispatch: dispatch,
                   action: /* BidAdd */Block.__(2, [6]),
                   label: "6",
                   id: "btnBid6",
                   color: "#606060",
-                  isVisible: state.isBiddingHideDenominationButtons
+                  isVisible: state.isBiddingHideDenominationButtons && (currentContractLevel <= 6 && currentContractSuitAsRank !== 4 || currentContractLevel < 6 && currentContractSuitAsRank === 4)
                 }), React.createElement(ButtonBidSmall$ReasonReactExamples.make, {
                   dispatch: dispatch,
                   action: /* BidAdd */Block.__(2, [7]),
                   label: "7",
                   id: "btnBid7",
                   color: "#606060",
-                  isVisible: state.isBiddingHideDenominationButtons
+                  isVisible: state.isBiddingHideDenominationButtons && (currentContractLevel <= 7 && currentContractSuitAsRank !== 4 || currentContractLevel < 7 && currentContractSuitAsRank === 4)
                 }), React.createElement(ButtonBidLarge$ReasonReactExamples.make, {
                   dispatch: dispatch,
                   action: /* BidAddSpecial */Block.__(4, ["Pass"]),
@@ -74,35 +133,35 @@ function BiddingWindow(Props) {
                   label: "\u2663",
                   id: "btnBidC",
                   color: "black",
-                  isVisible: !state.isBiddingHideDenominationButtons
+                  isVisible: !state.isBiddingHideDenominationButtons && (previousContractSuitAsRank < 0 || previousContractSuitAsRank === 4)
                 }), React.createElement(ButtonBidSmall$ReasonReactExamples.make, {
                   dispatch: dispatch,
                   action: /* BidUpdate */Block.__(3, ["Diamonds"]),
                   label: "\u2666",
                   id: "btnBidD",
                   color: "red",
-                  isVisible: !state.isBiddingHideDenominationButtons
+                  isVisible: !state.isBiddingHideDenominationButtons && (previousContractSuitAsRank < 1 || previousContractSuitAsRank === 4)
                 }), React.createElement(ButtonBidSmall$ReasonReactExamples.make, {
                   dispatch: dispatch,
                   action: /* BidUpdate */Block.__(3, ["Hearts"]),
                   label: "\u2665",
                   id: "btnBidH",
                   color: "red",
-                  isVisible: !state.isBiddingHideDenominationButtons
+                  isVisible: !state.isBiddingHideDenominationButtons && (previousContractSuitAsRank < 2 || previousContractSuitAsRank === 4)
                 }), React.createElement(ButtonBidSmall$ReasonReactExamples.make, {
                   dispatch: dispatch,
                   action: /* BidUpdate */Block.__(3, ["Spades"]),
                   label: "\u2660",
                   id: "btnBidS",
                   color: "black",
-                  isVisible: !state.isBiddingHideDenominationButtons
+                  isVisible: !state.isBiddingHideDenominationButtons && (previousContractSuitAsRank < 3 || previousContractSuitAsRank === 4)
                 }), React.createElement(ButtonBidSmall$ReasonReactExamples.make, {
                   dispatch: dispatch,
                   action: /* BidUpdate */Block.__(3, ["NoTrumps"]),
                   label: "NT",
                   id: "btnBidNT",
                   color: "#606060",
-                  isVisible: !state.isBiddingHideDenominationButtons
+                  isVisible: !state.isBiddingHideDenominationButtons && (previousContractSuitAsRank < 4 || previousContractSuitAsRank === 4)
                 }), React.createElement(SpacerStd$ReasonReactExamples.make, {
                   spacerWidth: "4vh"
                 }), React.createElement(ButtonBidLarge$ReasonReactExamples.make, {
--- src/Components/BiddingWindow.re
+++ src/Components/BiddingWindow.re
@@ -13,6 +13,75 @@ let make = (
   ;
   //Js.log("isShowXAndXX");
   //Js.log(isShowXAndXX);
+  // helper to decide highest bid level
+  let bidsListFilteredByContractLevel = Belt.List.keep(state.bids, x => {x.contractLevel !== None});
+  let optionCurrentContractLevelBid = Belt.List.head(bidsListFilteredByContractLevel);
+  let optionCurrentContractLevel =
+    switch (optionCurrentContractLevelBid) {
+      | None => None
+      | Some(x) => x.contractLevel
+    };
+  let currentContractLevel =
+    switch (optionCurrentContractLevel) {
+      | None => 0
+      | Some(n) => n
+    };
+  Js.log("currentContractLevel:");
+  Js.log(currentContractLevel);
+  // we need currentContractSuit too
+  let optionCurrentContractSuit =
+    switch (optionCurrentContractLevelBid) {
+      | None => None
+      | Some(x) => x.contractSuit
+    };
+  let currentContractSuitAsString = 
+    switch (optionCurrentContractSuit) {
+      | None => ""
+      | Some(x) => x
+    };
+  let currentContractSuitAsRank =
+    switch(currentContractSuitAsString) {
+      | "Clubs" => 0
+      | "Diamonds" => 1
+      | "Hearts" => 2
+      | "Spades" => 3
+      | "NoTrumps" => 4
+      | _ => -1
+    };
+  Js.log("currentContractSuitAsRank:");
+  Js.log(currentContractSuitAsRank);
+  // helper to decide highest denomination - we have to look at a previous bid record
+  let optionBidListTail = Belt.List.tail(bidsListFilteredByContractLevel);
+  let bidListTail =
+    switch (optionBidListTail) {
+      | None => []
+      | Some(x) => x
+    };
+  let optionPreviousBid = Belt.List.head(bidListTail);
+  let optionPreviousContractSuit =
+    switch (optionPreviousBid) {
+      | None => None
+      | Some(x) => x.contractSuit
+    };
+  let previousContractSuitAsString =
+    switch (optionPreviousContractSuit) {
+      | None => ""
+      | Some(x) => x
+    };
+  //Js.log("previousContractSuitAsString:");
+  //Js.log(previousContractSuitAsString);
+  let previousContractSuitAsRank =
+    switch(previousContractSuitAsString) {
+      | "Clubs" => 0
+      | "Diamonds" => 1
+      | "Hearts" => 2
+      | "Spades" => 3
+      | "NoTrumps" => 4
+      | _ => -1
+    };
+  Js.log("previousContractSuitAsRank:");
+  Js.log(previousContractSuitAsRank);
+
   <>
     //<span>(React.string("BiddingWindowComp"))</span>
     //(s2e("TEST"))
@@ -22,20 +91,76 @@ let make = (
     // Js.log({js|\u2666 3|js});
     // Js.log({js|\u2663 2|js});
     // Js.log({js|\u200A|js})
-    <ButtonBidSmall isVisible={state.isBiddingHideDenominationButtons} dispatch action=BidAdd(Some(1)) label="1" id="btnBid1" color="#606060"/>
-    <ButtonBidSmall isVisible={state.isBiddingHideDenominationButtons} dispatch action=BidAdd(Some(2)) label="2" id="btnBid2" color="#606060"/>
-    <ButtonBidSmall isVisible={state.isBiddingHideDenominationButtons} dispatch action=BidAdd(Some(3)) label="3" id="btnBid3" color="#606060"/>
-    <ButtonBidSmall isVisible={state.isBiddingHideDenominationButtons} dispatch action=BidAdd(Some(4)) label="4" id="btnBid4" color="#606060"/>
-    <ButtonBidSmall isVisible={state.isBiddingHideDenominationButtons} dispatch action=BidAdd(Some(5)) label="5" id="btnBid5" color="#606060"/>
-    <ButtonBidSmall isVisible={state.isBiddingHideDenominationButtons} dispatch action=BidAdd(Some(6)) label="6" id="btnBid6" color="#606060"/>
-    <ButtonBidSmall isVisible={state.isBiddingHideDenominationButtons} dispatch action=BidAdd(Some(7)) label="7" id="btnBid7" color="#606060"/>
+    <ButtonBidSmall isVisible={
+      state.isBiddingHideDenominationButtons 
+      && 
+      (
+        (currentContractLevel <= 1 && currentContractSuitAsRank !== 4)
+        ||
+        (currentContractLevel < 1 && currentContractSuitAsRank === 4)
+      )
+    } dispatch action=BidAdd(Some(1)) label="1" id="btnBid1" color="#606060"/>
+    <ButtonBidSmall isVisible={
+      state.isBiddingHideDenominationButtons 
+      && 
+      (
+        (currentContractLevel <= 2 && currentContractSuitAsRank !== 4)
+        ||
+        (currentContractLevel < 2 && currentContractSuitAsRank === 4)
+      )
+    } dispatch action=BidAdd(Some(2)) label="2" id="btnBid2" color="#606060"/>
+    <ButtonBidSmall isVisible={
+      state.isBiddingHideDenominationButtons 
+      && 
+      (
+        (currentContractLevel <= 3 && currentContractSuitAsRank !== 4)
+        ||
+        (currentContractLevel < 3 && currentContractSuitAsRank === 4)
+      )
+    } dispatch action=BidAdd(Some(3)) label="3" id="btnBid3" color="#606060"/>
+    <ButtonBidSmall isVisible={
+      state.isBiddingHideDenominationButtons 
+      && 
+      (
+        (currentContractLevel <= 4 && currentContractSuitAsRank !== 4)
+        ||
+        (currentContractLevel < 4 && currentContractSuitAsRank === 4)
+      )
+    } dispatch action=BidAdd(Some(4)) label="4" id="btnBid4" color="#606060"/>
+    <ButtonBidSmall isVisible={
+      state.isBiddingHideDenominationButtons 
+      && 
+      (
+        (currentContractLevel <= 5 && currentContractSuitAsRank !== 4)
+        ||
+        (currentContractLevel < 5 && currentContractSuitAsRank === 4)
+      )
+    } dispatch action=BidAdd(Some(5)) label="5" id="btnBid5" color="#606060"/>
+    <ButtonBidSmall isVisible={
+      state.isBiddingHideDenominationButtons 
+      && 
+      (
+        (currentContractLevel <= 6 && currentContractSuitAsRank !== 4)
+        ||
+        (currentContractLevel < 6 && currentContractSuitAsRank === 4)
+      )
+    } dispatch action=BidAdd(Some(6)) label="6" id="btnBid6" color="#606060"/>
+    <ButtonBidSmall isVisible={
+      state.isBiddingHideDenominationButtons 
+      && 
+      (
+        (currentContractLevel <= 7 && currentContractSuitAsRank !== 4)
+        ||
+        (currentContractLevel < 7 && currentContractSuitAsRank === 4)
+      )
+    } dispatch action=BidAdd(Some(7)) label="7" id="btnBid7" color="#606060"/>
     <ButtonBidLarge isVisible={state.isBiddingHideDenominationButtons} dispatch action=BidAddSpecial(Some("Pass")) label="Pass" id="btnBidPass" backgroundColor="#26653B"/>
     <br/>
-    <ButtonBidSmall isVisible={!state.isBiddingHideDenominationButtons} dispatch action=BidUpdate(Some("Clubs")) label={js|\u2663|js} id="btnBidC" color="black"/>
-    <ButtonBidSmall isVisible={!state.isBiddingHideDenominationButtons} dispatch action=BidUpdate(Some("Diamonds")) label={js|\u2666|js} id="btnBidD" color="red"/>
-    <ButtonBidSmall isVisible={!state.isBiddingHideDenominationButtons} dispatch action=BidUpdate(Some("Hearts")) label={js|\u2665|js} id="btnBidH" color="red"/>
-    <ButtonBidSmall isVisible={!state.isBiddingHideDenominationButtons} dispatch action=BidUpdate(Some("Spades")) label={js|\u2660|js} id="btnBidS" color="black"/>
-    <ButtonBidSmall isVisible={!state.isBiddingHideDenominationButtons} dispatch action=BidUpdate(Some("NoTrumps")) label="NT" id="btnBidNT" color="#606060"/>
+    <ButtonBidSmall isVisible={!state.isBiddingHideDenominationButtons && (previousContractSuitAsRank < 0 || previousContractSuitAsRank === 4)} dispatch action=BidUpdate(Some("Clubs")) label={js|\u2663|js} id="btnBidC" color="black"/>
+    <ButtonBidSmall isVisible={!state.isBiddingHideDenominationButtons && (previousContractSuitAsRank < 1 || previousContractSuitAsRank === 4)} dispatch action=BidUpdate(Some("Diamonds")) label={js|\u2666|js} id="btnBidD" color="red"/>
+    <ButtonBidSmall isVisible={!state.isBiddingHideDenominationButtons && (previousContractSuitAsRank < 2 || previousContractSuitAsRank === 4)} dispatch action=BidUpdate(Some("Hearts")) label={js|\u2665|js} id="btnBidH" color="red"/>
+    <ButtonBidSmall isVisible={!state.isBiddingHideDenominationButtons && (previousContractSuitAsRank < 3 || previousContractSuitAsRank === 4)} dispatch action=BidUpdate(Some("Spades")) label={js|\u2660|js} id="btnBidS" color="black"/>
+    <ButtonBidSmall isVisible={!state.isBiddingHideDenominationButtons && (previousContractSuitAsRank < 4 || previousContractSuitAsRank === 4)} dispatch action=BidUpdate(Some("NoTrumps")) label="NT" id="btnBidNT" color="#606060"/>
     <SpacerStd spacerWidth="4vh" />
     <ButtonBidLarge isVisible=isShowXAndXX dispatch action=BidAddSpecial(Some("X")) label="X" id="btnBidX" backgroundColor="#E00000"/>
     <ButtonBidLarge isVisible=isShowXAndXX dispatch action=BidAddSpecial(Some("XX")) label="XX" id="btnBidXX" backgroundColor="#000080"/>
