bedfaa8fe4941e4237b1d90cf7fa6b002c13a199
Jon Beckett <sales@jbds.co.uk>
2020-07-20 21:30:12 +0100

    v0.23 uses https and wss protocols

--- src/Index.bs.js
+++ src/Index.bs.js
@@ -16,7 +16,7 @@ const setVh = () => {
   window.userState = {}; // prevent undefined error before p5 starts up
 
   // be certain we have the correct version of bundled file aka index.js
-  document.title = 'Bridget v0.22';
+  document.title = 'Bridget v0.23';
 ;
 
 ReactDOMRe.renderToElementWithId(React.createElement(App$ReasonReactExamples.make, { }), "root");
