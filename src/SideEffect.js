a3f858af4cfc69efae5e14afb16ee371b5d20e31
Jon Beckett <sales@jbds.co.uk>
2020-09-07 17:18:21 +0100

    v0.36 active poc yellow

--- src/SideEffect.js
+++ src/SideEffect.js
@@ -21,33 +21,33 @@ let drawCards = (p, g, w, pointOfCompass) => {
     let userPointOfCompass = userPointOfCompassWrappedInArray[0].pointOfCompass;
     return (
       // every player can see their own hand
-      userPointOfCompass === poc 
-      || 
-      // observers can see all hands
-      userPointOfCompass === 'Observer' 
-      ||
-      // every player can see Dummy once lead card has been played
-      // check for poc equal to Dummy poc AND isDummyVisible
-      (
-        poc === getDummyPocByDeclarer(w.gameState.declarer)
-        &&
-        gameState.isDummyVisible === true
-      )
-      ||
-      // the dummy can observe the declarer's hand for interest only
-      (
-        poc === w.gameState.declarer
-        &&
-        userPointOfCompass === getDummyPocByDeclarer(w.gameState.declarer)
-      )
-      ? 
-      true 
-      : 
-      false
+      userPointOfCompass === poc
+        ||
+        // observers can see all hands
+        userPointOfCompass === 'Observer'
+        ||
+        // every player can see Dummy once lead card has been played
+        // check for poc equal to Dummy poc AND isDummyVisible
+        (
+          poc === getDummyPocByDeclarer(w.gameState.declarer)
+          &&
+          gameState.isDummyVisible === true
+        )
+        ||
+        // the dummy can observe the declarer's hand for interest only
+        (
+          poc === w.gameState.declarer
+          &&
+          userPointOfCompass === getDummyPocByDeclarer(w.gameState.declarer)
+        )
+        ?
+        true
+        :
+        false
     );
   };
   // helper function to see if this is the Dummy hand
-  let isDummyHand = 
+  let isDummyHand =
     pointOfCompass === getDummyPocByDeclarer(w.gameState.declarer) ? true : false;
   // fetch 1/4 of the card pack or less
   switch (pointOfCompass) {
@@ -56,57 +56,57 @@ let drawCards = (p, g, w, pointOfCompass) => {
       //g.isHandFaceUp = gameState.handVisible.north;
       g.isHandFaceUp = shouldHandBeUp('North') || gameState.handVisible.north;
       g.myHandArray = window.gameState.pack.filter(obj => {
-          return (obj.shuffleIndex >= 0 && obj.shuffleIndex <=12 && obj.lifecycle === 1)
-        }
-      ); 
+        return (obj.shuffleIndex >= 0 && obj.shuffleIndex <= 12 && obj.lifecycle === 1)
+      }
+      );
       clockPosition = rotationPlusPointOfCompassToClockPosition(
         w.userState.tableRotationDegrees,
         'North'
-        );
+      );
       translateAndRotateByClockPosition(clockPosition, p, g, isDummyHand);
-    break;
+      break;
     case 'East':
       //g.isHandFaceUp = gameState.handVisible.east;
       g.isHandFaceUp = shouldHandBeUp('East') || gameState.handVisible.east;
       g.myHandArray = window.gameState.pack.filter(obj => {
-          return (obj.shuffleIndex >= 13 && obj.shuffleIndex <=25 && obj.lifecycle === 1)
-        }
-      ); 
+        return (obj.shuffleIndex >= 13 && obj.shuffleIndex <= 25 && obj.lifecycle === 1)
+      }
+      );
       clockPosition = rotationPlusPointOfCompassToClockPosition(
         w.userState.tableRotationDegrees,
         'East'
-        );
-        translateAndRotateByClockPosition(clockPosition, p, g, isDummyHand);
-    break;
+      );
+      translateAndRotateByClockPosition(clockPosition, p, g, isDummyHand);
+      break;
     case "South":
       //g.isHandFaceUp = gameState.handVisible.south;
       g.isHandFaceUp = shouldHandBeUp('South') || gameState.handVisible.south;
       g.myHandArray = window.gameState.pack.filter(obj => {
-          return (obj.shuffleIndex >= 26 && obj.shuffleIndex <=38 && obj.lifecycle == 1)
-        }
+        return (obj.shuffleIndex >= 26 && obj.shuffleIndex <= 38 && obj.lifecycle == 1)
+      }
       );
       clockPosition = rotationPlusPointOfCompassToClockPosition(
         w.userState.tableRotationDegrees,
         'South'
-        );
-        translateAndRotateByClockPosition(clockPosition, p, g, isDummyHand);
-    break;
+      );
+      translateAndRotateByClockPosition(clockPosition, p, g, isDummyHand);
+      break;
     case 'West':
       //g.isHandFaceUp = gameState.handVisible.west;
       g.isHandFaceUp = shouldHandBeUp('West') || gameState.handVisible.west;
       g.myHandArray = window.gameState.pack.filter(obj => {
-          return (obj.shuffleIndex >= 39 && obj.shuffleIndex <=51 && obj.lifecycle == 1)
-        }
-      ); 
+        return (obj.shuffleIndex >= 39 && obj.shuffleIndex <= 51 && obj.lifecycle == 1)
+      }
+      );
       clockPosition = rotationPlusPointOfCompassToClockPosition(
         w.userState.tableRotationDegrees,
         'West'
-        );
-        translateAndRotateByClockPosition(clockPosition, p, g, isDummyHand);
-    break;
+      );
+      translateAndRotateByClockPosition(clockPosition, p, g, isDummyHand);
+      break;
     case 'Discard':
       g.myDiscardArray = window.gameState.pack.filter(obj => {
-          return (obj.lifecycle === 2 )
+        return (obj.lifecycle === 2)
       });
       break;
     default:
@@ -127,39 +127,39 @@ let paintHandArray = (p, g) => {
   // NB visible segment has 20% added for overlap on all except last card
   let cardVisibleSegmentWidthToCardWidthRatio = 37 / 216;
   // 13 records or less - need to reorder by handOrder
-  g.myHandArray.sort((a, b) => {return b.handOrder - a.handOrder});
+  g.myHandArray.sort((a, b) => { return b.handOrder - a.handOrder });
   // offset the start of card drawing dependent upon qty cards in hand
-  p.translate(-((g.myHandArray.length / 2.0))* cardWidth * cardVisibleSegmentWidthToCardWidthRatio, 0);
+  p.translate(-((g.myHandArray.length / 2.0)) * cardWidth * cardVisibleSegmentWidthToCardWidthRatio, 0);
   g.myHandArray.forEach((obj, i) => {
     // now we can draw a card using each fileName in myHandArray
     let p5img = g.isHandFaceUp ? g.imgMap.get(obj.fileName) : g.imgMap.get('1B');
     // last card in hand is a special case, as we do not want the overlap
     i !== g.myHandArray.length - 1
-    ?
-    p.image(
-      p5img, 
-      0, 
-      -cardHeight * g.cardSegmentHeightToCardRatio, 
-      cardWidth * cardVisibleSegmentWidthToCardWidthRatio * 1.2, 
-      cardHeight * g.cardSegmentHeightToCardRatio,
-      0,
-      0, 
-      37 * 1.2, 
-      91
-    )
-    :
-    p.image(
-      p5img, 
-      0, 
-      -cardHeight * g.cardSegmentHeightToCardRatio, 
-      cardWidth * cardVisibleSegmentWidthToCardWidthRatio, 
-      cardHeight * g.cardSegmentHeightToCardRatio,
-      0,
-      0, 
-      37, 
-      91
-    )
-    ;
+      ?
+      p.image(
+        p5img,
+        0,
+        -cardHeight * g.cardSegmentHeightToCardRatio,
+        cardWidth * cardVisibleSegmentWidthToCardWidthRatio * 1.2,
+        cardHeight * g.cardSegmentHeightToCardRatio,
+        0,
+        0,
+        37 * 1.2,
+        91
+      )
+      :
+      p.image(
+        p5img,
+        0,
+        -cardHeight * g.cardSegmentHeightToCardRatio,
+        cardWidth * cardVisibleSegmentWidthToCardWidthRatio,
+        cardHeight * g.cardSegmentHeightToCardRatio,
+        0,
+        0,
+        37,
+        91
+      )
+      ;
     // note that each rotation is cumulative
     //p.rotate(rotationDeltaRadians);
     // note that each translation is cumulative
@@ -179,52 +179,52 @@ let paintDiscardArray = (p, g, w) => {
     // position of card depends on range of shuffleIndex and rotation
     // but these two variables are orthogonal
     p.push();
-      switch (w.userState.tableRotationDegrees) {
-        case 0: 
-          p.rotate(0); 
-          break;
-        case 90: 
-          p.rotate(p.HALF_PI);
-          break;
-        case 180:
-          p.rotate(p.HALF_PI * 2);
-          break;
-        case 270:
-          p.rotate(p.HALF_PI * 3);
-          break;
-        default:
-          console.log('Unexpected tableRotation argument ');
-      }
-      switch (true) {
-        // N
-        case (obj.shuffleIndex < 13):
-          // good for all rotations, because x=x and y=y all angles
-          p.translate(0, -cardHeight * cardHeightOffsetFraction);
-          break;
-        // E
-        case (obj.shuffleIndex < 26):
-          p.translate(cardWidth * cardWidthOffsetFraction, 0);
-          break;
-        // S
-        case (obj.shuffleIndex < 39):
-          p.translate(0, cardHeight * cardHeightOffsetFraction);
+    switch (w.userState.tableRotationDegrees) {
+      case 0:
+        p.rotate(0);
         break;
-        // W
-        case (obj.shuffleIndex < 52):
-          p.translate(-cardWidth * cardWidthOffsetFraction, 0);
-          break;
-        default:
+      case 90:
+        p.rotate(p.HALF_PI);
+        break;
+      case 180:
+        p.rotate(p.HALF_PI * 2);
+        break;
+      case 270:
+        p.rotate(p.HALF_PI * 3);
+        break;
+      default:
+        console.log('Unexpected tableRotation argument ');
+    }
+    switch (true) {
+      // N
+      case (obj.shuffleIndex < 13):
+        // good for all rotations, because x=x and y=y all angles
+        p.translate(0, -cardHeight * cardHeightOffsetFraction);
+        break;
+      // E
+      case (obj.shuffleIndex < 26):
+        p.translate(cardWidth * cardWidthOffsetFraction, 0);
+        break;
+      // S
+      case (obj.shuffleIndex < 39):
+        p.translate(0, cardHeight * cardHeightOffsetFraction);
+        break;
+      // W
+      case (obj.shuffleIndex < 52):
+        p.translate(-cardWidth * cardWidthOffsetFraction, 0);
+        break;
+      default:
         console.log('Unexpected shuffleIndex argument');
-      }
-      let p5img = g.imgMap.get(obj.fileName);
-      p.image(p5img, -cardWidth / 2, -cardHeight / 2, cardWidth, cardHeight);
+    }
+    let p5img = g.imgMap.get(obj.fileName);
+    p.image(p5img, -cardWidth / 2, -cardHeight / 2, cardWidth, cardHeight);
     p.pop();
   });
 };
 
 let drawLabels = (p, g, w) => {
   // avoid error 
-  if(!w.gameState.pointOfCompassAndPlayers) {
+  if (!w.gameState.pointOfCompassAndPlayers) {
     //console.log('No labels defined, abort drawLabels');
     return;
   }
@@ -243,39 +243,39 @@ let drawLabels = (p, g, w) => {
       let clockPosition = rotationPlusPointOfCompassToClockPosition(
         w.userState.tableRotationDegrees,
         obj.pointOfCompass
-        );
+      );
       p.push();
       // translate and rotate by clock position
-      switch(clockPosition) {
+      switch (clockPosition) {
         case '12PM':
-          p.translate(0, -(g.canvasHeight / 2)  + cardSegmentHeight + (textHeightToCanvasHeightRatio * 0.75));
+          p.translate(0, -(g.canvasHeight / 2) + cardSegmentHeight + (textHeightToCanvasHeightRatio * 0.75));
           //p.rotate(p.HALF_PI * 2); 
-        break;
+          break;
         case '3PM':
           p.translate((g.canvasWidth / 2) - cardSegmentHeight - (textHeightToCanvasHeightRatio * 0.75), 0);
           p.rotate(p.HALF_PI * 3);
-        break;
+          break;
         case '6PM':
-          p.translate(0, (g.canvasHeight / 2)  - cardSegmentHeight - (textHeightToCanvasHeightRatio * 0.75));
-          p.rotate(0); 
-        break;
+          p.translate(0, (g.canvasHeight / 2) - cardSegmentHeight - (textHeightToCanvasHeightRatio * 0.75));
+          p.rotate(0);
+          break;
         case '9PM':
           p.translate((-g.canvasWidth / 2) + cardSegmentHeight + (textHeightToCanvasHeightRatio * 0.75), 0);
           p.rotate(p.HALF_PI);
-        break;
+          break;
         default:
           console.log('Unexpected clockPosition argument');
       };
       // do not draw label if pointOfCompass is not known
       // add dealer if appropriate
-      let positionPlayerDealer; 
+      let positionPlayerDealer;
       if (w.gameState.dealer === obj.pointOfCompass && w.gameState.declarer === obj.pointOfCompass) {
         positionPlayerDealer = 'Dealer + Declarer ' + obj.pointOfCompass + ' ' + obj.player
       } else if (w.gameState.dealer === obj.pointOfCompass && getDummyPocByDeclarer(w.gameState.declarer) === obj.pointOfCompass) {
         positionPlayerDealer = 'Dealer + Dummy ' + obj.pointOfCompass + ' ' + obj.player
       } else if (w.gameState.dealer === obj.pointOfCompass) {
-        positionPlayerDealer = 'Dealer ' + obj.pointOfCompass + ' ' + obj.player  
-      } else if (w.gameState.declarer === obj.pointOfCompass) { 
+        positionPlayerDealer = 'Dealer ' + obj.pointOfCompass + ' ' + obj.player
+      } else if (w.gameState.declarer === obj.pointOfCompass) {
         positionPlayerDealer = 'Declarer ' + obj.pointOfCompass + ' ' + obj.player
       } else if (getDummyPocByDeclarer(w.gameState.declarer) === obj.pointOfCompass) {
         positionPlayerDealer = 'Dummy ' + obj.pointOfCompass + ' ' + obj.player
@@ -283,19 +283,20 @@ let drawLabels = (p, g, w) => {
         positionPlayerDealer = obj.pointOfCompass + ' ' + obj.player;
       }
       if (w.gameState.activePointOfCompass === obj.pointOfCompass) {
-        // white, 12% transparent
-        p.fill(255, 240);
+        // was white, 12% transparent p.fill(255, 240)
+        // now yellow, 0% transparent (ref Jill)
+        p.fill(0, 255, 255, 0);
       } else {
         // white, 62% transparent
         p.fill(255, 96);
       }
-      if(obj.pointOfCompass != '') {
+      if (obj.pointOfCompass != '') {
         p.text(
-          positionPlayerDealer, 
-          -g.canvasWidth / 4, 
+          positionPlayerDealer,
+          -g.canvasWidth / 4,
           -textHeightToCanvasHeightRatio,
           g.canvasWidth / 2,
-          textHeightToCanvasHeightRatio * 2 
+          textHeightToCanvasHeightRatio * 2
         );
       } else {
         // skip
@@ -308,56 +309,56 @@ let drawLabels = (p, g, w) => {
 // HELPER FUNCTION TO CALCULATE CLOCK POSITION
 // (tableRotationDegrees (0, 90 etc), pointOfCompass ('North' etc)) =>
 // clockPosition ('12PM' etc)
-let rotationPlusPointOfCompassToClockPosition = 
+let rotationPlusPointOfCompassToClockPosition =
   (tableRotationDegrees, pointOfCompass) => {
-  let pointOfCompassDegrees = () => {
-    switch(pointOfCompass) {
-      case 'North':
-        return 0;
-        break;
-      case 'East':
-        return 90;
-        break;
-      case 'South':
-        return 180;
-        break;
-      case 'West':
-        return 270
-        break;
-      default:
-        // this can occur by design before players are assigned
-        //console.log('Unexpected pointOfCompass argument');
-        return null;
+    let pointOfCompassDegrees = () => {
+      switch (pointOfCompass) {
+        case 'North':
+          return 0;
+          break;
+        case 'East':
+          return 90;
+          break;
+        case 'South':
+          return 180;
+          break;
+        case 'West':
+          return 270
+          break;
+        default:
+          // this can occur by design before players are assigned
+          //console.log('Unexpected pointOfCompass argument');
+          return null;
+      };
     };
-  };
-  let degrees = (tableRotationDegrees + pointOfCompassDegrees()) % 360;
-  //console.log(degrees);
-  let clockPosition = () => {
-    switch (degrees){
-      case 0:
-        return '12PM';
-        break;
-      case 90:
-        return '3PM';
-        break;
-      case 180:
-        return '6PM';
-        break;
-      case 270:
-        return '9PM';
-        break;
-      default:
-        console.log('Unexpected degrees argument');
-        return null;
+    let degrees = (tableRotationDegrees + pointOfCompassDegrees()) % 360;
+    //console.log(degrees);
+    let clockPosition = () => {
+      switch (degrees) {
+        case 0:
+          return '12PM';
+          break;
+        case 90:
+          return '3PM';
+          break;
+        case 180:
+          return '6PM';
+          break;
+        case 270:
+          return '9PM';
+          break;
+        default:
+          console.log('Unexpected degrees argument');
+          return null;
+      };
     };
+    //console.log(clockPosition());
+    return clockPosition();
   };
-  //console.log(clockPosition());
-  return clockPosition();
-};
 
 // HELPER FUNCTION FOR drawCards
 let translateAndRotateByClockPosition = (clockPosition, p, g, isDummyHand) => {
-  switch(clockPosition) {
+  switch (clockPosition) {
     case '12PM':
       // isDummyHand 
       // ? 
@@ -373,19 +374,19 @@ let translateAndRotateByClockPosition = (clockPosition, p, g, isDummyHand) => {
       // tweak offset appropriately
       p.translate(0, -(g.canvasHeight / 2) + (g.canvasHeight * g.cardHeightToCanvasHeightRatio * g.cardSegmentHeightToCardRatio));
       // no rotation!
-    break;
+      break;
     case '3PM':
       p.translate((g.canvasWidth / 2), 0);
       p.rotate(p.HALF_PI * 3);
-    break;
+      break;
     case '6PM':
       p.translate(0, (g.canvasHeight / 2));
-      p.rotate(0); 
-    break;
+      p.rotate(0);
+      break;
     case '9PM':
       p.translate((-g.canvasWidth / 2), 0);
       p.rotate(p.HALF_PI);
-    break;
+      break;
     default:
       console.log('Unexpected clockPosition argument');
   };
@@ -394,10 +395,10 @@ let translateAndRotateByClockPosition = (clockPosition, p, g, isDummyHand) => {
 // HELPER FUNC FOR drawLabels
 let getDummyPocByDeclarer = (poc) => {
   switch (poc) {
-    case 'West': 
+    case 'West':
       return 'East';
       break;
-    case 'North': 
+    case 'North':
       return 'South';
       break;
     case 'East':
@@ -414,10 +415,10 @@ let getDummyPocByDeclarer = (poc) => {
 // HELPER FUNC FOR drawCards
 let getLeadPointOfCompassFromDeclarer = (poc) => {
   switch (poc) {
-    case 'West': 
+    case 'West':
       return 'North';
       break;
-    case 'North': 
+    case 'North':
       return 'East';
       break;
     case 'East':
