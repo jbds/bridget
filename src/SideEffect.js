5fabf1ea17b616b7c222e70fb6ed29de26911526
Jon Beckett <sales@jbds.co.uk>
2020-11-13 20:29:09 +0000

    v2.01

--- src/SideEffect.js
+++ src/SideEffect.js
@@ -470,7 +470,7 @@ let paintDiscardArray = (p, g, w) => {
     let cardVisibleSegmentWidthToCardWidthRatio = 37 / 216;
     if (
       (userState.tableRotationDegrees == 0 && gameState.declarer == 'East') ||
-      (userState.tableRotationDegrees == 90 && gameState.declarer == 'North') ||
+      (userState.tableRotationDegrees == 90 && gameState.declarer == 'North') //||
       //(userState.tableRotationDegrees == 180 && gameState.declarer == 'West') //||
       // (userState.tableRotationDegrees == 270 && gameState.declarer == 'South')
     ) {
