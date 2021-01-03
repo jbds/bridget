1e6d78dbc1925ba528a95331e6a0fb0c3dc3879e
Jon Beckett <sales@jbds.co.uk>
2021-01-03 15:25:18 +0000

    v3.78

--- src/SideEffect.js
+++ src/SideEffect.js
@@ -487,8 +487,10 @@ let paintDiscardArray = (p, g, w) => {
           w.userState.tableRotationDegrees,
           'East'
         );
-        if (false) {
-
+        if (w.gameState.discardPocForTransition == 'East' && clockPosition == '12PM' && getDummyPocByDeclarer(w.gameState.declarer) == 'East') {
+          w.gameState.transition.eastStartX = p.lerp(w.gameState.transition.eastStartX, w.gameState.transition.eastEndX, lerpDelta);
+          w.gameState.transition.eastStartYInv = p.lerp(w.gameState.transition.eastStartYInv, w.gameState.transition.eastEndY, lerpDelta);
+          p.translate(w.gameState.transition.eastStartX * w.innerHeight, w.gameState.transition.eastStartYInv * w.innerHeight);
         } else {
           w.gameState.transition.eastStartX = p.lerp(w.gameState.transition.eastStartX, w.gameState.transition.eastEndX, lerpDelta);
           w.gameState.transition.eastStartY = p.lerp(w.gameState.transition.eastStartY, w.gameState.transition.eastEndY, lerpDelta);
