66ea9949e839cd44edc1943467254937a4648872
Jon Beckett <sales@jbds.co.uk>
2021-01-24 13:48:30 +0000

    v4.16

--- src/Index.bs.js
+++ src/Index.bs.js
@@ -16,7 +16,7 @@ const setVh = () => {
   window.userState = {}; // prevent undefined error before p5 starts up
 
   // be certain we have the correct version of bundled file aka index.js
-  document.title = 'Bridget v4.15';
+  document.title = 'Bridget v4.16';
 
   // for transitions we need to move card constants up to global scope
   // std playing card is 3.5in x 2.25in and svg is 336px x 216px
